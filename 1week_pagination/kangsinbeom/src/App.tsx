b964a10de19567cd07a602e491dadae2184f9a76
kangsinbeom <kanian16@naver.com>
2024-02-20 11:58:19 +0900

    'layout: total layout'

---
+++ 1week_pagination/kangsinbeom/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Flex, ItemList, Pagination } from "./components/shared";
+import Spacing from "./components/shared/Spacing";
+
+function App() {
+  return (
+    <Flex direction="column" align="center">
+      <ItemList />
+      <Spacing size={80} />
+      <Spacing size={2} backgroundColor="lightgray" style={{ width: "100%" }} />
+      <Spacing size={80} />
+      <Pagination limit={101} />
+    </Flex>
+  );
+}
+
+export default App;
