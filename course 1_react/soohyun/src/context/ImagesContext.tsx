c4dd7d428b397453d2ebc246f275ddcd71f1d0ed
soohyun <gamjatan9@gmail.com>
2024-02-22 20:56:49 +0900

    Feat: images 최적화

--- course 1_react/soohyun/src/context/ImagesContext.tsx
+++ course 1_react/soohyun/src/context/ImagesContext.tsx
@@ -1,19 +1,13 @@
-import React, { createContext, useContext, Dispatch, SetStateAction, useState, ReactNode } from 'react';
+import React, { createContext, useContext, useEffect, useState, ReactNode } from 'react';
 import { useFetchData } from '../hooks/useFetchData';
-
-// 이미지 데이터 타입
-interface Image {
-  id: string;
-  url: string;
-}
 
 // context에 전달될 값
 interface ImagesContextType {
-  images: Image[];
+  images: { id: string; url: string; }[];
   isLoading: boolean;
   error: Error | null;
   currentPage: number; 
-  setCurrentPage: Dispatch<SetStateAction<number>>; 
+  setCurrentPage: (page: number) => void;
   totalPages: number; 
 }
 
@@ -21,9 +15,18 @@ const ImagesContext = createContext<ImagesContextType | null>(null);
 
 
 export const ImagesProvider = ({ children }: { children: ReactNode }) => {
-  const [currentPage, setCurrentPage] = useState(1); //초기값
+  // 로컬 스토리지에서 현재 페이지 번호를 읽어옴
+  const initialPage = Number(localStorage.getItem('currentPage')) || 1;
+  const [currentPage, setCurrentPage] = useState(initialPage);
+
   const totalPages = 20; 
+
   const { data: images, isLoading, error } = useFetchData(currentPage);
+
+  // 현재 페이지 번호가 변경될 때마다 로컬 스토리지를 업데이트
+  useEffect(() => {
+    localStorage.setItem('currentPage', currentPage.toString());
+  }, [currentPage]);
 
   return (
     <ImagesContext.Provider value={{ images, isLoading, error, currentPage, setCurrentPage, totalPages }}>
@@ -34,7 +37,7 @@ export const ImagesProvider = ({ children }: { children: ReactNode }) => {
 
 export const useImages = (): ImagesContextType => {
   const context = useContext(ImagesContext);
-  if (context === null) {
+  if (!context) {
     throw new Error('error: useImages');
   }
   return context;
